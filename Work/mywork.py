0fab1e296cd73899083f2186d3ad737b3b8d4e66
mohawk2846 <super_aleks9@ukr.net>
2022-12-10 22:33:26 +0200

    just

---
+++ Work/mywork.py
@@ -0,0 +1,46 @@
+
+# import time
+
+# print("Hello,my name Sasha.\nWhats your name?")
+# time.sleep(4)
+# name1 = input("Whats your name?")
+# print ("Nice to meet you: " + name1)
+
+
+#import random
+#print(random.randint(0,100))
+
+#a = 4
+#b = 2
+#print (a%b)
+
+# import random 
+
+# random_number = random.randint(1,2)
+# user_number = input("Вгагадй число від 1 до 7 : ")
+
+# if int(user_number) == random_number :
+#     print("Ти вгадава!!!")
+# else :
+#     print("Ти не вгадав")
+#     print(f"Загадане число було {random_number}")
+
+# age = input("Enter how old are you : ")
+
+# if int(age) >= 19 :
+#     print("You old-ass")
+# else :
+#     print ("You young people")
+
+# import random 
+# random_number = random.randint(1,2)
+# random_user = input("1 - 2 NUMBER??????")
+
+# if int(random_user) == random_number :
+#     print("You lucky")
+#     print(f"true,number {random_number}")
+# else :
+#     print ("You dont lucky")
+#     print(f"number {random_number}")
+
+
